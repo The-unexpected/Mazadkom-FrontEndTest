0816e1eb5414f3a1b5fd9c2423fea72cff2cbd85
dana-younis <younisdana7@gmail.com>
2021-07-30 18:33:24 +0300

    product

--- mazadkom-front/src/App.js
+++ mazadkom-front/src/App.js
@@ -1,12 +1,47 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-export default class App extends Component {
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>hello !</h1>
+//       </div>
+//     )
+//   }
+// }
+
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Welcome from './component/product/Welcome';
+import './App.css';
+
+
+class App extends Component {
+  // Initialize state
+  state = { details: [], bidHistory: [] };
+  // Fetch liveStock Details after first mount
+  componentDidMount() {
+    this.getDetails();
+  }
+  getDetails = () => {
+    // Get the livestock Details and store them in state
+    fetch('/api/details')
+      .then((res) => res.json())
+      .then((details) => this.setState({ details }));
+  };
   render() {
     return (
-      <div>
-        <h1>hello !</h1>
+      <div className="App container-fluid">
+        <div className="row nav-bar">
+          <div className="col-md-12 app-title">
+            <h1>BUYMYCOW</h1>
+          </div>
+        </div>
+        <div className="row">
+          <Welcome details={this.state.details} />
+        </div>
       </div>
-    )
+    );
   }
 }
-
+export default App;
---
+++ mazadkom-front/src/component/product/BidHistory.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+class BidHistory extends Component {
+  render() {
+    let self = this;
+    let historyList = Object.keys(this.props.bidHistory).map(function (key) {
+      return (
+        <li className="list-group-item" key={key}>
+          <span>{key}</span>{' '}
+          <span style={{ float: 'right' }}>${self.props.bidHistory[key]}</span>
+        </li>
+      );
+    });
+    return (
+      <div className="col-md-12">
+        <ul className="bid-history-list list-group">
+          <li className="list-group-item">
+            <span>Bidder</span>{' '}
+            <span style={{ float: 'right' }}>Bid Price</span>
+          </li>
+          {historyList}
+        </ul>
+      </div>
+    );
+  }
+}
+export default BidHistory;
