46fa385f6439c8e6e3a31e17093a3efe0dd55988
MohammadAltamimi98 <m98altamimi@gmail.com>
2021-08-04 17:18:09 +0300

    commit

--- mazadkom-front/src/components/Room.js
+++ mazadkom-front/src/components/Room.js
@@ -7,35 +7,29 @@ import Monaliza from "./image/monaliza.jpg";
 class Room extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       messages: [],
       startPrice: 100,
-      totalPrice: 0,
-      click1_count: '',
+      click_count: 100,
       username: '',
-
       count: 0,
-
     };
     this.sendMessage = this.sendMessage.bind(this);
-    this.increaseBy20 = this.increaseBy20.bind(this);
+    this.increaseBy50 = this.increaseBy50.bind(this);
     this.increaseBy100 = this.increaseBy100.bind(this);
     this.increaseBy200 = this.increaseBy200.bind(this);
   }
 
   componentDidMount() {
-    this.socket = io('https://mazadkom.herokuapp.com');
+    this.socket = io('http://localhost:5000');
     this.socket.on('message', (message) => {
       this.setState({
         messages: [message, ...this.state.messages],
       });
     });
-    this.socket.emit('room111', () => {
-      console.log('room111');
-    })
 
-    this.socket.on('counterroom111', (count) => {
+
+    this.socket.on('counter', (count) => {
       this.setState({
         count: count,
         ...this.state.count,
@@ -48,18 +42,14 @@ class Room extends React.Component {
   sendMessage(event) {
     const body = event.target.value;
     const id = localStorage.getItem('id');
-    axios.get(`https://mazadkom.herokuapp.com/user/${id}`).then(res => {
+    axios.get(`http://localhost:5000/user/${id}`).then(res => {
       let response = JSON.parse(JSON.stringify(res));
       console.log("response", response);
-
       this.setState({
         username: response.data.UserInfo[0].username,
-
       })
-
       console.log(this.state.username);
       return res;
-
     })
 
     // if pressing enter button and body exsit
@@ -75,32 +65,33 @@ class Room extends React.Component {
     }
   }
 
-  increaseBy20(e) {
-    this.socket.emit('clickedroom111', this.increaseBy20); //Emitting user click
-    this.socket.on('click1_count', (value) => {
+  increaseBy50(e) {
+    this.socket.emit('clicked', this.increaseBy50); //Emitting user click
+    this.socket.on('click_count', (value) => {
       console.log('value', value);
       this.setState({
-        click1count: value,
+        click_count: value,
       });
     });
   }
 
+
   increaseBy100(e) {
-    this.socket.emit('clicked1room111', this.increaseBy100); //Emitting user click
-    this.socket.on('click1_count', (value) => {
+    this.socket.emit('clicked1', this.increaseBy100); //Emitting user click
+    this.socket.on('click_count', (value) => {
       console.log('value', value);
       this.setState({
-        click1_count: value,
+        click_count: value,
       });
     });
   }
 
   increaseBy200(e) {
-    this.socket.emit('clicked2room111', this.increaseBy200); //Emitting user click
-    this.socket.on('click1_count', (value) => {
+    this.socket.emit('clicked2', this.increaseBy200); //Emitting user click
+    this.socket.on('click_count', (value) => {
       console.log('value', value);
       this.setState({
-        click1_count: value,
+        click_count: value,
       });
     });
   }
@@ -108,7 +99,7 @@ class Room extends React.Component {
   render() {
     return (
       <div className="App">
-        <button onClick={this.increaseBy20}>increase 50$</button>
+        <button onClick={this.increaseBy50}>increase 50$</button>
         <button onClick={this.increaseBy100}>increase 100$</button>
         <button onClick={this.increaseBy200}>increase 200$</button>
 
@@ -125,16 +116,17 @@ class Room extends React.Component {
           );
         })}
         <h1>Counter = {this.state.count}</h1>
-        <h1 id="counterroom111"> total={this.state.click1_count}</h1>
+
+        <h1 id="counterroom111"> total={this.state.click_count}</h1>
 
         <Card className="image-card">
-          <Card.Img variant="top"  src={Monaliza} />
+          <Card.Img variant="top" src={Monaliza} />
           <Card.Body>
             <Card.Title>Portrait of a Musician</Card.Title>
             <Card.Text>
               The Portrait of a Musician is an unfinished painting widely attributed to the Italian Renaissance artist Leonardo da Vinci, dated to circa 1483–1487. Produced while Leonardo was in Milan, the work is painted in oils, and perhaps tempera, on a small panel of walnut wood.
             </Card.Text>
-            <Card.Text>Price : {this.state.startPrice+this.state.click1_count}$
+            <Card.Text>Price : {this.state.startPrice + this.state.click_count}$
             </Card.Text>
           </Card.Body>
 
