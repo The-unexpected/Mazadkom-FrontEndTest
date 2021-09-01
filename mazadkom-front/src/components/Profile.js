70e28e5bd8aa101e4838635736cb519096ee2a9e
MohammadAltamimi98 <m98altamimi@gmail.com>
2021-09-01 18:23:23 +0300

    commit

--- mazadkom-front/src/components/Profile.js
+++ mazadkom-front/src/components/Profile.js
@@ -1,83 +1,53 @@
-import axios from 'axios';
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import CardGroup from 'react-bootstrap/CardGroup';
-import Card from 'react-bootstrap/Card';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "react-bootstrap/Card";
 
+function Profile() {
+  const [username, setUsername] = useState("");
+  // const [image, setImage] = useState("");
+  // const [title, setTitle] = useState("");
+  // const [description, setDescription] = useState("");
+  const [bids, setBids] = useState([]);
 
-export default class Profile extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      image: '',
-      title: '',
-      description: '',
-      bids: []
-
-    }
-  }
-
-
-
-  componentDidMount() {
-    const id = localStorage.getItem('id');
+  useEffect((props) => {
+    const id = localStorage.getItem("id");
     console.log("id=", id);
     try {
-      const request = axios.get(`https://mazadkom.herokuapp.com/user/${id}`).then(res => {
-        let response = JSON.parse(JSON.stringify(res));
-        console.log(response);
-
-        this.setState({
-          username: response.data.UserInfo[0].username,
-          image: response.data.UserInfo[0].image,
-          bids: response.data.UserInfo[0].bids[0],
-          // title: response.data.UserInfo[0].bids[0].title,
-          // description: response.data.UserInfo[0].bids[0].description,
-          // price: response.data.UserInfo[0].bids[0].startingPrice,
-        })
-        // console.log('bids', this.state.bids.title);
-        // console.log('bids', this.state.bids.description);
-        // console.log('bids', this.state.bids.startingPrice);
-        // console.log('bids', this.state.price);
-        // console.log('bids', this.state.bids.length);
-
-        return res;
-      })
-    }
-    catch (e) {
+      const request = axios
+        .get(`https://mazadkom.herokuapp.com/user/${id}`)
+        .then((res) => {
+          let response = JSON.parse(JSON.stringify(res));
+          console.log(response);
+          setUsername(response.data.UserInfo[0].username);
+          // setImage(response.data.UserInfo[0].image);
+          setBids(response.data.UserInfo[0].bids[0]);
+        });
+    } catch (e) {
       console.log(e.message);
     }
+  }, []);
+  return (
+    <div>
+      <h1>Greetings {username} ! </h1>
 
-  }
-
-
-
-  render() {
-    return (
-      <div>
-        <h1>Greetings {this.state.username} ! </h1>
-
-        {this.state.bids &&
-          <Card
-            bg="primary"
-            text="white"
-            style={{ width: '18rem' }}
-            className="mb-2"
-          >
-            <Card.Header>Claimed bid</Card.Header>
-            <Card.Body>
-              <Card.Title>{this.state.bids.title} </Card.Title>
-              <Card.Text>
-                {this.state.bids.description}
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>{this.state.bids.startingPrice}</Card.Footer>
-          </Card>
-        }
-      </div>
-    )
-
-  }
+      {bids && (
+        <Card
+          bg="primary"
+          text="white"
+          style={{ width: "18rem" }}
+          className="mb-2"
+        >
+          <Card.Header>Claimed bid</Card.Header>
+          <Card.Body>
+            <Card.Title>{bids.title} </Card.Title>
+            <Card.Text>{bids.description}</Card.Text>
+          </Card.Body>
+          <Card.Footer>{bids.startingPrice}</Card.Footer>
+        </Card>
+      )}
+    </div>
+  );
 }
+
+export default Profile;
