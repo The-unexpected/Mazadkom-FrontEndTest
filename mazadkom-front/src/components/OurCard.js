65fb1e70ed28aea22b0df715d440b151890e4d52
MohammadAltamimi98 <m98altamimi@gmail.com>
2021-09-06 18:51:17 +0300

    commit

--- mazadkom-front/src/components/OurCard.js
+++ mazadkom-front/src/components/OurCard.js
@@ -1,22 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { Card, Row, Col, Button, CardGroup } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 import "./css/OurCard.css";
+import { UserContext } from "../context/context";
+import { useHistory } from "react-router-dom";
 
 function OurCard(props) {
+  const { user, setUser, value, setValue } = useContext(UserContext);
+  let history = useHistory();
   const [data, setData] = useState([]);
-  const [username, setUsername] = useState('')
-  const [title, setTitle] = useState('')
+  const [username, setUsername] = useState("");
+  const [title, setTitle] = useState("");
 
   useEffect((props) => {
     try {
-      const name = localStorage.getItem('username');
+      const name = localStorage.getItem("username");
       setUsername(name);
       axios.get(`http://localhost:5000/apiElement`).then((res) => {
         let response = JSON.parse(JSON.stringify(res.data.productElementInfo));
         setData(response);
-
 
         console.log(response);
       });
@@ -28,11 +31,9 @@ function OurCard(props) {
   return (
     <div className="container mb-5 mt-5 pt-5 pb-5">
       <CardGroup className="container mb-5 mt-5 pt-5 pb-5">
-
-
         {/* <Card className="image-card"> */}
         <Row xs={1} md={3} className="g-4">
-          {data.map((element,idx) => {
+          {data.map((element, idx) => {
             return (
               <div className="cards">
                 {/* // <Row xs={1} md={2} className="g-4"> */}
@@ -47,7 +48,6 @@ function OurCard(props) {
                     <Card.Title>{element.title}</Card.Title>
                     <Card.Text>
                       {element._id}
-
                       <p>{element.description} </p> <br />
                       <p>{element.startingPrice}</p>
                     </Card.Text>
@@ -55,30 +55,30 @@ function OurCard(props) {
                   <Button
                     className="button"
                     variant="outline-secondary"
-
                     onClick={() => {
-                      localStorage.setItem('header', idx);
+                      setValue(element._id);
+                      console.log(value);
+                      localStorage.setItem("header", idx);
+                      history.push(`/room?name=${username}&room=${idx}`);
                     }}
-                    href={`/room?name=${username}&room=${idx}`}
 
+                    // href={`/room?name=${username}&room=${idx}`}
                   >
                     Join Room
                   </Button>{" "}
                   {/* </Card> */}
                 </Col>
-
               </div>
-
             );
-
           })}
         </Row>
 
+        {/* 
+        
 
-
+ */}
         {/* </Card> */}
-
-      </CardGroup >
+      </CardGroup>
 
       {/* <Col>
           <Card className="image-card">
@@ -124,7 +124,7 @@ function OurCard(props) {
           </Card> */}
       {/* </Col> */}
       {/* </Row> */}
-    </div >
+    </div>
   );
 }
 
